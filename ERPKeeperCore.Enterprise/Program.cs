f24f2877ecc6ad2ca20942ec7964fa7c6e4b26fc
Pawin Yousook <101880729+sonteenboy@users.noreply.github.com>
2024-11-03 13:16:14 +0700

    fdf

--- ERPKeeperCore.Enterprise/Program.cs
+++ ERPKeeperCore.Enterprise/Program.cs
@@ -31,45 +31,16 @@ namespace ERPKeeperCore.CMD
 
                 var oldOrganization = new ERPKeeper.Node.DAL.Organization(enterpriseDB, true);
 
-
-                newOrganization.SaleQuotes.RemoveExpired(360);
-                newOrganization.Customers.RemoveNoSaleOrQuote();
-
-
-                newOrganization.PurchaseQuotes.RemoveExpired(360);
-
-                newOrganization.Suppliers.GetAll().ForEach(s =>
+                newOrganization.Sales.GetAll().ForEach(s =>
                 {
-                    if (s.Purchases.Count() == 0 && s.PurchaseQuotes.Count() == 0)
-                    {
-                        Console.WriteLine($"Supplier {s.Profile.Name} has no purchases or purchase quotes");
-                        s.SupplierItems.Clear();
-                        s.Profile.Supplier = null;
-
-                        newOrganization.ErpCOREDBContext.Suppliers.Remove(s);
-
-                        newOrganization.SaveChanges();
-                    }
-                });
-
-
-
-
-                newOrganization.Customers.GetAll().ForEach(s =>
-                {
-                    if (s.Sales.Count() == 0 && s.SaleQuotes.Count() == 0)
-                    {
-                        Console.WriteLine($"Customer {s.Profile.Name} has no sales or sales quotes");
-                        s.CustomerItems.Clear();
-                        s.Profile.Customer = null;
-
-                        newOrganization.ErpCOREDBContext.Customers.Remove(s);
-
-                        newOrganization.SaveChanges();
-                    }
-
+                    s.UnPostLedger();
+                    s.IncomeAccount = newOrganization.SystemAccounts.Income;
+                    s.Customer.DefaultIncomeAccount = newOrganization.SystemAccounts.Income;
+                    s.TaxCode = newOrganization.TaxCodes.GetDefault(Enterprise.Models.Taxes.Enums.TaxDirection.Output);
+                    s.Customer.DefaultTaxCode = newOrganization.TaxCodes.GetDefault(Enterprise.Models.Taxes.Enums.TaxDirection.Output);
                 });
                 newOrganization.SaveChanges();
+                newOrganization.Sales.PostToTransactions();
 
                 //var eRPMigrationTool = new ERPMigrationTool(enterpriseDB);
                 //eRPMigrationTool.Migrate();
