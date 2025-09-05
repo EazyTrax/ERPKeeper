4a8bbf0a1a6034417a192df35dd57484e3d173db
Pawin Yousook <pawin.y@engineeringcraft.com>
2025-09-05 09:27:51 +0700

    d

---
+++ ERPKeeperCore/Migrations/20250905022742_DB-2025.9.5.1.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ERPKeeperCore.Enterprise.Migrations
+{
+    /// <inheritdoc />
+    public partial class DB2025951 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsApproved",
+                table: "SaleQuotes",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "SaleQuotes",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsPriceTaxInclude",
+                table: "SaleQuotes",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsApproved",
+                table: "SaleQuotes");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "SaleQuotes");
+
+            migrationBuilder.DropColumn(
+                name: "IsPriceTaxInclude",
+                table: "SaleQuotes");
+        }
+    }
+}
