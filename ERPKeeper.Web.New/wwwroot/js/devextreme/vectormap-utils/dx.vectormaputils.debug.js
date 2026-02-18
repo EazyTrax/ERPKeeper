70fa4534da31b7d975360c649594e3b908d7da58
Pawin Yousook <pawin.y@engineeringcraft.com>
2026-02-18 17:29:50 +0700

    รn

--- ERPKeeper.Web.New/wwwroot/js/devextreme/vectormap-utils/dx.vectormaputils.debug.js
+++ ERPKeeper.Web.New/wwwroot/js/devextreme/vectormap-utils/dx.vectormaputils.debug.js
@@ -1,9 +1,9 @@
 /*!
  * DevExtreme (dx.vectormaputils.debug.js)
- * Version: 24.2.7
- * Build date: Mon Apr 28 2025
+ * Version: 25.2.4
+ * Build date: Mon Jan 26 2026
  *
- * Copyright (c) 2012 - 2025 Developer Express Inc. ALL RIGHTS RESERVED
+ * Copyright (c) 2012 - 2026 Developer Express Inc. ALL RIGHTS RESERVED
  * Read about DevExtreme licensing here: https://js.devexpress.com/Licensing/
  */
 "use strict";
